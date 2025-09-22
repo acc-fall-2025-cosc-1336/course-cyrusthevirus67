84d83a7818bc134ed289252a3a86af2d5771888f
cyrusthevirus67 <cyrus.paul@g.austincc.edu>
2025-09-22 16:26:55 +0000

    hw01: add multiply_numbers function, main program, tests, and pytest config

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,9 @@
-#
+from src.homework.b_in_proc_out.output import multiply_numbers
+
+def main() -> None:
+    # Required outputs
+    print(f"7 x 7 = {multiply_numbers(7, 7)}")
+    print(f"5 x 5 = {multiply_numbers(5, 5)}")
+
+if __name__ == "__main__":
+    main()
